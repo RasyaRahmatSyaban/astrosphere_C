5bbbb87a51f2a50a7b97ffaac4a71a3a25c88032
RasyaRahmatSyaban <rasyarahmatsyban10@gmail.com>
2024-03-23 22:31:09 +0800

    add new folder

---
+++ materi/week10/index.js
@@ -0,0 +1,45 @@
+// const mobil = {
+//     warna : "merah",
+//     "jenis Mesin" : "diesel",
+//     kapasitasBensin : 200
+// };
+
+// // const mobil.warna = "hitam";
+
+// const{kapasitasBensin} = mobil;
+
+// console.log(mobil.warna)
+// console.log(mobil ["jenis Mesin"])
+// console.log(kapasitasBensin)
+
+// const pesawat = {
+//     crew : {
+//         captain : {
+//             name : "Rasya",
+//             age : 20
+//         },
+//         pramugari : {
+//             name : "Cahya",
+//             age : 20
+//         },
+//         pramugura : {
+//             name : "caa",
+//             age : 18
+//         },
+//     }
+// }
+// console.log(pesawat.crew.captain.name)
+// console.log(pesawat.crew.pramugura)
+// console.log(pesawat.crew.pramugari)
+
+
+// const motor ={
+//     gas(kec){
+//         if (kec > 60){
+//         console.log(`motor melaju secepat ${kec}` )
+//     }else if (kec < 60){
+//         console.log(`aihh lambat, cuma ${kec}` )
+//     }
+// }
+// };
+// motor.gas(5500)
